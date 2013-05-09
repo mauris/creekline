4ff49cb50ba2e36f48cb6dc4c96239fa489e31b9
Sam-Mauris Yong <mauris@hotmail.sg>
2013-05-09 20:49:11 +0800

    repository factory and interface

---
+++ src/Creekline/Repository/Repository.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Creekline
+ * By Sam-Mauris Yong
+ * 
+ * Released open source under New BSD 3-Clause License.
+ * Copyright (c) 2012, Sam-Mauris Yong Shan Xian <sam@mauris.sg>
+ * All rights reserved.
+ */
+
+namespace Creekline\Repository;
+
+/**
+ * Repository class
+ *
+ * The manager for downloading and running tests for a package
+ *
+ * @author Sam-Mauris Yong / mauris@hotmail.sg
+ * @copyright Copyright (c) 2013, Sam-Mauris Yong
+ * @license http://www.opensource.org/licenses/bsd-license New BSD License
+ * @package Creekline\Repository
+ * @since 1.0.0
+ */
+class Repository implements RepositoryInterface {
+    
+}
