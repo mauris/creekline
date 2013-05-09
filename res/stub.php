4941778020fa780b3bc7420f1a5911766e7349a7
Sam-Mauris Yong <mauris@hotmail.sg>
2013-05-09 20:19:41 +0800

    bootstrappers

---
+++ res/stub.php
@@ -0,0 +1,16 @@
+#!/usr/bin/env php
+<?php
+
+/**
+ * Creekline
+ * By Sam-Mauris Yong
+ * 
+ * Released open source under New BSD 3-Clause License.
+ * Copyright (c) Sam-Mauris Yong Shan Xian <sam@mauris.sg>
+ * All rights reserved.
+ */
+
+Phar::mapPhar('creekline.phar');
+require 'phar://creekline.phar/bin/creekline';
+
+__HALT_COMPILER();
