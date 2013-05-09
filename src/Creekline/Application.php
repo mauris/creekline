aeb5690ff45702c25ad08114e32e5d110a1a3ee8
Sam-Mauris Yong <mauris@hotmail.sg>
2013-05-09 20:48:55 +0800

    main application implementation

---
+++ src/Creekline/Application.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Creekline
+ * By Sam-Mauris Yong
+ * 
+ * Released open source under New BSD 3-Clause License.
+ * Copyright (c) 2012, Sam-Mauris Yong Shan Xian <sam@mauris.sg>
+ * All rights reserved.
+ */
+
+namespace Creekline;
+
+/**
+ * Application class
+ *
+ * The main application
+ *
+ * @author Sam-Mauris Yong / mauris@hotmail.sg
+ * @copyright Copyright (c) 2013, Sam-Mauris Yong
+ * @license http://www.opensource.org/licenses/bsd-license New BSD License
+ * @package Creekline
+ * @since 1.0.0
+ */
+class Application {
+    
+    public function run($args){
+        
+    }
+    
+}
