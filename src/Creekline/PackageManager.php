bced729a801d020219598a810a35fef382e85051
Sam-Mauris Yong <mauris@hotmail.sg>
2013-05-09 20:49:04 +0800

    packagemanager implementation

---
+++ src/Creekline/PackageManager.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Creekline
+ * By Sam-Mauris Yong
+ * 
+ * Released open source under New BSD 3-Clause License.
+ * Copyright (c) 2012, Sam-Mauris Yong Shan Xian <sam@mauris.sg>
+ * All rights reserved.
+ */
+
+namespace Creekline;
+
+/**
+ * PackageManager class
+ *
+ * The manager for downloading and running tests for a package
+ *
+ * @author Sam-Mauris Yong / mauris@hotmail.sg
+ * @copyright Copyright (c) 2013, Sam-Mauris Yong
+ * @license http://www.opensource.org/licenses/bsd-license New BSD License
+ * @package Creekline
+ * @since 1.0.0
+ */
+class PackageManager {
+    
+    /**
+     *
+     * @var \Creekline\Repository\RepositoryInterface
+     */
+    private $repository;
+    
+        
+    public function __construct($repository){
+        
+    }
+    
+}
