55c3cda79fbd69eefb188ec66214526e07964233
Sam-Mauris Yong <mauris@hotmail.sg>
2013-05-10 22:29:30 +0800

    moved out MockProcess

--- test/Creekline/Repository/GitTest.php
+++ test/Creekline/Repository/GitTest.php
@@ -22,7 +22,7 @@ class GitTest extends \PHPUnit_Framework_TestCase
     protected function setUp()
     {
         $this->object = new Git('https://github.com/thephpdeveloper/creekline.git');
-        $this->processor = '\\Creekline\\Repository\\MockProcess';
+        $this->processor = '\\Creekline\\MockProcess';
     }
 
     /**
