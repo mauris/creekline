051c5dd6a3b85b46786385c6779e2ec21d0f28d6
Sam-Mauris Yong <mauris@hotmail.sg>
2013-05-10 22:44:19 +0800

    MockComposerProcess implementation

---
+++ test/Creekline/MockComposerProcess.php
@@ -0,0 +1,33 @@
+<?php
+namespace Creekline;
+
+class MockComposerProcess {
+    
+    private $cmd;
+    
+    public function __construct($cmd){
+        $this->cmd = $cmd;
+    }
+    
+    public function run(){
+        return 0;
+    }
+    
+    public function getOutput(){
+        return <<<EOF
+Loading composer repositories with package information
+Updating dependencies (including require-dev)
+  - Removing symfony/process (v2.1.6)
+  - Installing symfony/process (v2.2.1)
+    Loading from cache
+
+  - Removing symfony/finder (v2.1.6)
+  - Installing symfony/finder (v2.2.1)
+    Downloading: 100%
+
+Writing lock file
+Generating autoload files
+EOF;
+    }
+    
+}
