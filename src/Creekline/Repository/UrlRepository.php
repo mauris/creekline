c3a59c5dcd2fee63f1ecd890892eceea7aa24d27
Sam-Mauris Yong <mauris@hotmail.sg>
2013-05-10 16:53:47 +0800

    renamed and created abstract urlrepository

---
+++ src/Creekline/Repository/UrlRepository.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Creekline
+ * By Sam-Mauris Yong
+ * 
+ * Released open source under New BSD 3-Clause License.
+ * Copyright (c) 2012, Sam-Mauris Yong Shan Xian <sam@mauris.sg>
+ * All rights reserved.
+ */
+
+namespace Creekline\Repository;
+
+/**
+ * UrlRepository class
+ *
+ * A repository that handles a URL
+ *
+ * @author Sam-Mauris Yong / mauris@hotmail.sg
+ * @copyright Copyright (c) 2013, Sam-Mauris Yong
+ * @license http://www.opensource.org/licenses/bsd-license New BSD License
+ * @package Creekline\Repository
+ * @since 1.0.0
+ */
+abstract class UrlRepository implements RepositoryInterface {
+    
+    public abstract function __construct($url);
+
+}
