5012ea1865391f91852aa711c155e4f77e9254e1
Sam-Mauris Yong <mauris@hotmail.sg>
2013-05-12 00:29:12 +0800

    Config loading class

---
+++ src/Creekline/Config/Config.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * Creekline
+ * By Sam-Mauris Yong
+ * 
+ * Released open source under New BSD 3-Clause License.
+ * Copyright (c) 2012, Sam-Mauris Yong Shan Xian <sam@mauris.sg>
+ * All rights reserved.
+ */
+
+namespace Creekline\Config;
+
+use Packfire\Config\ConfigFactory;
+
+/**
+ * Config class
+ *
+ * Configuration processing class
+ *
+ * @author Sam-Mauris Yong / mauris@hotmail.sg
+ * @copyright Copyright (c) 2013, Sam-Mauris Yong
+ * @license http://www.opensource.org/licenses/bsd-license New BSD License
+ * @package Creekline\Config
+ * @since 1.0.0
+ */
+class Config {
+    
+    private $projects;
+    
+    public function __construct($data){
+        $this->sort($data);
+    }
+    
+    protected function sort($data){
+        if(isset($data['projects'])){
+            $this->projects = $data['projects'];
+        }
+    }
+    
+    public function projects(){
+        return $this->projects;
+    }
+    
+    public static function load($file){
+        $factory = new ConfigFactory();
+        return new self($factory->load($file));
+    }
+    
+}
